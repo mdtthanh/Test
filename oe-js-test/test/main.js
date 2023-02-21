e4326bd6f4ee619dec4b23bf9121ae79b9171aeb
mdtthanh <maithanh0131@gmail.com>
2023-02-22 03:31:53 +0700

    add

--- oe-js-test/test/main.js
+++ oe-js-test/test/main.js
@@ -2,22 +2,19 @@ fetch("../data/products.json")
     .then(response => response.json())
     .then(data => {
         let products = data.products
-        // console.log(products)
         let placeholder = document.querySelector(".column2__row");
-        let drink = "";
         let topp = []
         let typeTopping = "";
         for (let product of products) {
             topp.push(...product.toppings)
         }
         let topps = [...new Set(topp)]
-        // console.log(topps)
 
         for (let topp of topps) {
             typeTopping += `
                 <div class="filter__toppings-toggle--items">
-                    <input class="filter__toppings-toggle--input" type="checkbox" name="topping" id="topping" value="${topp}">
-                    <label class="filter__toppings-toggle--label" for="">${topp}</label>
+                    <input class="filter__toppings-toggle--input" type="checkbox" name="topping" id="topping-${topp}" value="${topp}">
+                    <label class="filter__toppings-toggle--label" for="topping-${topp}">${topp}</label>
                 </div>
             `
         }
@@ -28,43 +25,46 @@ fetch("../data/products.json")
         checkboxes.forEach((checkbox) => {
             checkbox.addEventListener('click', filterProduct);
         });
+
         function filterProduct() {
             const filters = [];
             const checkedBoxes = document.querySelectorAll('input[type=checkbox]:checked');
             checkedBoxes.forEach((checkbox) => {
                 const value = checkbox.value;
                 filters.push(value);
-            })
-            console.log(filters)
-
-            const result = products.filter(product => {
-                return product.toppings && product.toppings.length && filters.every(topping => product.toppings.includes(topping));
             });
-            console.log(result)
-            
-            for (let product of result) {
-                drink += `
-                    <div class="column2__card" >
-                        <div class="column2__card-items">
-                            <p class="column2__card-items--id">MT-0${product.id}</p>
-                            <p class="column2__card-items--name">${product.name}</p>
-                            <p class="column2__card-items--topping">Topping</p>
-                            <span class="column2__card-items--topping--name">${product.toppings}</span>
-                            <p class="column2__card-items--price">$${product.price}</p>
-                        </div>
-                    </div>
-            `
-            }
-            placeholder.innerHTML = drink;
 
-            // if (filters.length = 0) {
-            //     appearProduct(products)
-            //     console.log(data.products)
-            // }
-            // else {
-            //     appearProduct(result)
-            // }
+            let filteredProducts;
+            if (filters.length === 0) {
+                filteredProducts = products;
+            } else {
+                filteredProducts = products.filter(product => {
+                    return product.toppings && product.toppings.length && filters.every(topping => product.toppings.includes(topping));
+                });
+            }
+
+            if (filteredProducts.length === 0) {
+                // Display a message or hide the products section
+                console.log('No products match the selected filters');
+            } else {
+                let drink = "";
+                for (let product of filteredProducts) {
+                    drink += `
+                        <div class="column2__card" >
+                            <div class="column2__card-items">
+                                <p class="column2__card-items--id">MT-0${product.id}</p>
+                                <p class="column2__card-items--name">${product.name}</p>
+                                <p class="column2__card-items--topping">Topping</p>
+                                <span class="column2__card-items--topping--name">${product.toppings}</span>
+                                <p class="column2__card-items--price">$${product.price}</p>
+                            </div>
+                        </div>
+                    `
+                }
+                placeholder.innerHTML = drink;
+            }
         }
+
 
     })
 
