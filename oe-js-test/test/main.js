b95e9994ebe7f691cb5567c4ecbdedb245a0dcaf
mdtthanh <maithanh0131@gmail.com>
2023-02-22 03:06:18 +0700

    add

--- oe-js-test/test/main.js
+++ oe-js-test/test/main.js
@@ -8,61 +8,63 @@ fetch("../data/products.json")
         let topp = []
         let typeTopping = "";
         for (let product of products) {
-            drink += `
-                <div class="column2__card">
-                    <div class="column2__card-items">
-                        <p class="column2__card-items--id">MT-0${product.id}</p>
-                        <p class="column2__card-items--name">${product.name}</p>
-                        <p class="column2__card-items--topping">Topping</p>
-                        <span class="column2__card-items--topping--name">${product.toppings}</span>
-                        <p class="column2__card-items--price">$${product.price}</p>
-                    </div>
-                </div>
-        `
             topp.push(...product.toppings)
         }
-        placeholder.innerHTML = drink;
-        // console.log(topp)
         let topps = [...new Set(topp)]
         // console.log(topps)
 
         for (let topp of topps) {
             typeTopping += `
                 <div class="filter__toppings-toggle--items">
-                    <input class="filter__toppings-toggle--input" type="checkbox" id="topping" value="">
-                    <label class="filter__toppings-toggle--label" for="topping">${topp}</label>
+                    <input class="filter__toppings-toggle--input" type="checkbox" name="topping" id="topping" value="${topp}">
+                    <label class="filter__toppings-toggle--label" for="">${topp}</label>
                 </div>
             `
         }
         document.querySelector(".filter__toppings").innerHTML = typeTopping;
 
-        let a = ["Milk foam", "White pearl"]
-        let toppingFilter = []
-        let bs = products
+        const checkboxes = document.querySelectorAll('input[type=checkbox]');
 
-        for (let a1 of a) {
-            for (let b of bs) {
-                let i = 0
-                for (let topp of b.toppings) {
-                    if (topp === a1) {
-                        i++;
-                    }
-                }
-                if (i === 0) {
-                    bs.pop(b)
-                }
+        checkboxes.forEach((checkbox) => {
+            checkbox.addEventListener('click', filterProduct);
+        });
+        function filterProduct() {
+            const filters = [];
+            const checkedBoxes = document.querySelectorAll('input[type=checkbox]:checked');
+            checkedBoxes.forEach((checkbox) => {
+                const value = checkbox.value;
+                filters.push(value);
+            })
+            console.log(filters)
+
+            const result = products.filter(product => {
+                return product.toppings && product.toppings.length && filters.every(topping => product.toppings.includes(topping));
+            });
+            console.log(result)
+            
+            for (let product of result) {
+                drink += `
+                    <div class="column2__card" >
+                        <div class="column2__card-items">
+                            <p class="column2__card-items--id">MT-0${product.id}</p>
+                            <p class="column2__card-items--name">${product.name}</p>
+                            <p class="column2__card-items--topping">Topping</p>
+                            <span class="column2__card-items--topping--name">${product.toppings}</span>
+                            <p class="column2__card-items--price">$${product.price}</p>
+                        </div>
+                    </div>
+            `
             }
+            placeholder.innerHTML = drink;
+
+            // if (filters.length = 0) {
+            //     appearProduct(products)
+            //     console.log(data.products)
+            // }
+            // else {
+            //     appearProduct(result)
+            // }
         }
-        console.log(bs)
 
     })
 
-function handleToggle() {
-    var x = document.querySelector(".filter__toppings");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-
-    } else {
-        x.style.display = "none";
-    }
-}
